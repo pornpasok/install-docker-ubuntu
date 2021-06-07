32eb3d256adfaf8fdb7c520f99cbd90dc9c04129
PORNPASOK SOOKYEN <ton@PORNPASOKs-MacBook-Air.local>
2021-06-07 14:18:26 +0700

    add docker_ubuntu1804.sh

--- docker_ubuntu1804.sh
+++ docker_ubuntu1804.sh
@@ -8,7 +8,7 @@ sudo apt-get install \
     ca-certificates \
     curl \
     gnupg \
-    lsb-release
+    lsb-release -y
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
@@ -16,10 +16,16 @@ echo \
   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get update -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y 
 
 sudo usermod -aG docker ${USER}
 
 # Test docker run from user ubuntu
 docker run hello-world
+
+# docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
+
