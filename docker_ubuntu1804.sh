facbabba7f52c4869f5f69834a63c944d9f9fd0b
PORNPASOK SOOKYEN <ton@PORNPASOKs-MacBook-Air.local>
2021-06-07 14:35:54 +0700

    add docker_ubuntu1804.sh

--- docker_ubuntu1804.sh
+++ docker_ubuntu1804.sh
@@ -20,11 +20,13 @@ sudo apt-get update -y
 sudo apt-get install docker-ce docker-ce-cli containerd.io -y 
 
 sudo usermod -aG docker ${USER}
+# Active Group
+newgrp docker
 
 # Test docker run from user ubuntu
 docker run hello-world
 
-# docker-compose
+# Install docker-compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
